2edcee311e5fd58767563f276808e14fa7fb4604
balajiki1 <balaji.ki@northeastern.edu>
2025-02-28 12:58:47 -0500

    cusotmer script changes

--- SQL scripts/1_customer_script.sql
+++ SQL scripts/1_customer_script.sql
@@ -6,6 +6,7 @@ EXEC BUSINESS_MANAGER.show_returns_request('alice@gmail.com');
 -- customer views the rating of all stores before making a return
 SELECT * FROM BUSINESS_MANAGER.store_average_rating_view;
 
+
 -- Customer initiates return
 --create_return(reason, quantity_returned, store_id, order_product_id)
 VARIABLE Available_Qty NUMBER;
